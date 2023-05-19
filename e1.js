3321571cb3add4e2054bf19110360a4134f3e04c
Rahul Reddy <90768989+Bo5sE6S@users.noreply.github.com>
2023-05-18 22:55:11 -0400

    Added new buttons for humidity, temperature, and pm

--- e1.js
+++ e1.js
@@ -13,11 +13,59 @@ function initializeMap() {
   heatmap.setMap(map);
 }
 
-async function addHeatmapData() {
+async function addtempData() {
   var heatmapData = [];
   
   try {
-    const response = await fetch('https://pollution-patrol-maps-api.uc.r.appspot.com/show');
+    const response = await fetch('https://pollution-patrol-maps-api.uc.r.appspot.com/temp');
+    const data = await response.json();
+
+    for (var i = 0; i < data.length; i++) {
+      var lat = data[i].location.lat;
+      var lng = data[i].location.lng;
+      var we = data[i].weight;
+      var extracted = new google.maps.LatLng(lat, lng);
+      var newEntry = { location: extracted, weight: we };
+      heatmapData.push(newEntry);
+    }
+
+    // update heatmap data
+    heatmap.setData(new google.maps.MVCArray(heatmapData));
+
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function addpmData() {
+  var heatmapData = [];
+  
+  try {
+    const response = await fetch('https://pollution-patrol-maps-api.uc.r.appspot.com/pm');
+    const data = await response.json();
+
+    for (var i = 0; i < data.length; i++) {
+      var lat = data[i].location.lat;
+      var lng = data[i].location.lng;
+      var we = data[i].weight;
+      var extracted = new google.maps.LatLng(lat, lng);
+      var newEntry = { location: extracted, weight: we };
+      heatmapData.push(newEntry);
+    }
+
+    // update heatmap data
+    heatmap.setData(new google.maps.MVCArray(heatmapData));
+
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+async function addhumidData() {
+  var heatmapData = [];
+  
+  try {
+    const response = await fetch('https://pollution-patrol-maps-api.uc.r.appspot.com/humid');
     const data = await response.json();
 
     for (var i = 0; i < data.length; i++) {
